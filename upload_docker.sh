6c04c1733d175c14fdf4e0e095c95809c71fd8b7
seiyamiyaoka <seiyamiyaoka@gmail.com>
2019-08-04 09:32:11 +0900

    add outpufile and edit upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -10,6 +10,11 @@ dockerpath=seiyamiyaoka/house-prediction
 
 # Step 2:
 # Authenticate & tag
+# Login
+echo "Longin start"
+docker tag seiyamiyaoka/house-prediction seiyamiyaoka/house-prediction:0.1
+# add tag
+docker tag ${dockerpath} ${dockerpath}:0.1
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
